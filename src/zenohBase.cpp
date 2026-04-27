87c41e822f98707d548e5530298530a8c66aca9b
rob42 <robert@42.co.nz>
2026-04-27 15:03:22 +1200

    Added remove key from readings

--- src/zenohBase.cpp
+++ src/zenohBase.cpp
@@ -251,9 +251,14 @@ void baseInit(const char *hostname, const char *rsyslog, PicoSyslog::LogLevel le
 void expireReadings(){
   syslog.debug.println("expireReadings");
   for (JsonPair kv : readings.as<JsonObject>()) {
+    unsigned long timeout = kv.value()[KEY_TIMEOUT].as<long>();
     syslog.debug.println(kv.key().c_str());  
     syslog.debug.println(kv.value()[KEY_VALUE].as<long>()); 
-    syslog.debug.println(kv.value()[KEY_TIMEOUT].as<long>()); 
+    syslog.debug.println(timeout); 
+    if(millis() > timeout + 1000 ){
+      readings.remove(kv.key());
+      syslog.debug.println("removed");
+    }
   }
 
 
